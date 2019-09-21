c05ef355ef1e38e08e7f9244bcb2d375c123a814
tirth-hihoriya <18bce244@nirmauni.ac.in>
2019-09-21 20:52:37 +0530

    t2b

--- LineCoding_TirthHihoriya.py
+++ LineCoding_TirthHihoriya.py
@@ -1,17 +1,17 @@
-  
-def NRZ_L(b):
+from utilities import text2binary
+def NRZ_L(message):
     output1 =[] 
-    for i in range(0,len(input)):
+    for i in text2binary(message):
         if b[i]==0:
             output1.append(1)
         else:
             output1.append(-1)
     return output1
     
-def NRZ_I(b):
+def NRZ_I(message):
     output2 = []
     flag = 1
-    for i in b:
+    for i in text2binary(message):
         if(i == 1):
             if(flag == 1):
                 flag = -1
