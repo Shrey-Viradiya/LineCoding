c0017a02f5a9207c5a6fcda608167de280e26258
tirthasheshpatel <tirthasheshpatel@gmail.com>
2019-09-21 19:09:19 +0530

    Fix image to binary to output binary string

--- utilities.py
+++ utilities.py
@@ -6,4 +6,8 @@ def text2binary(string):
 
 def image_to_binary(data):
     ret, bw_img = cv2.threshold(data,127,255,cv2.THRESH_BINARY)
-    return bw_img
+    img_str = ''
+    for i in range(bw_img.shape[0]):
+        for j in range(bw_img.shape[1]):
+            img_str += '{0:07b}'.format(bw_img[i][j])
+    return img_str
