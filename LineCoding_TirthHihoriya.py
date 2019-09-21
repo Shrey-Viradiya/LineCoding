a305d104105c009fabf065dc64e227079d33a5a8
Shrey Viradiya <shrey.viradiya.100@outlook.com>
2019-09-21 19:52:25 +0530

    changes

--- LineCoding_TirthHihoriya.py
+++ LineCoding_TirthHihoriya.py
@@ -1,36 +1,27 @@
-input = [0,1,0,0,1,1,1,0,1,0]    # only to test
- 
-
-output1 =[]     # only to test
+  
 def NRZ_L(b):
+    output1 =[] 
     for i in range(0,len(input)):
         if b[i]==0:
             output1.append(1)
         else:
             output1.append(-1)
+    return output1
     
-
-output2 =[]   # only to test
-flag = 1
 def NRZ_I(b):
-    for i in range(0,len(input)):
-        if(b[0]==0):
-            output2.append(1)
-        elif(b[0]==1):
-            output2.append(-1)
-        elif(b[i]==0):
-            output2.append(flag)
-        elif(b[i]==1):
-            if(flag==1):
-                flag=-1
+    output2 = []
+    flag = 1
+
+    for i in b:
+        if(i == 1):
+            if(flag == 1):
+                flag = -1
             else:
-                flag=1
-
+                flag = 1
             output2.append(flag)
+        else:
+            output2.append(flag)
+    return output2
 
-NRZ_L(input)  
-print(output1)       
-NRZ_I(input)
-print(output2)
 
         
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utilities import text2binary
 from linecoding_shrey import polarRZ, AMI, pseudoternary
 
-print(text2binary('Hello World'))
-for x in pseudoternary('Hello World'):
+print(text2binary('Hello'))
+for x in pseudoternary("Hello"):
     print(x, end="")
