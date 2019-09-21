62bb812bc850370d0e3448a8ed4af20a76fcfd30
Shrey Viradiya <shrey.viradiya.100@outlook.com>
2019-09-21 18:27:51 +0530

    basic utilitie added

---
+++ main.py
@@ -0,0 +1,4 @@
+from utilities import text2binary
+
+print(text2binary('Hello World'))
+print(type(text2binary('Hello World')))
---
+++ utilities.py
@@ -0,0 +1,3 @@
+def text2binary(string):
+    output = ' '.join(format(ord(x), 'b') for x in string)
+    return output
