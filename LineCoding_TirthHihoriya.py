6542775cdfcb012ae2bcacab0eec7ab5d78bd4ca
tirth-hihoriya <18bce244@nirmauni.ac.in>
2019-09-22 07:29:59 +0530

    comment manchester

--- LineCoding_TirthHihoriya.py
+++ LineCoding_TirthHihoriya.py
@@ -84,9 +84,11 @@ def manchester(message):
     output = []
     for x in text2binary(message):
         if (x == '0'):
-            output.append(1) # 1 -> 1 and -1
-        else:
-            output.append(-1) # -1 -> -1 and 1
-    return output  
+            output.append(1) # 1 -> 1 and -1       or we can do  output.append(1)
+        else:                               #                    output.append(-1)
+
+            output.append(-1) # -1 -> -1 and 1                   output.append(-1)
+    return output                           #                    output.append(1)
+                                            #  and then while ploting, we will set the interval as half(0.5)
             
 
