fee0f854080371d318133031b2f58d8afa490fd2
Shrey Viradiya <shrey.viradiya.100@outlook.com>
2019-10-15 18:00:54 +0530

    tkinter

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
 from utilities import text2binary
+from line_coding_schemes import AMI
+from PIL import ImageTk
+import PIL.Image
 from tkinter import *
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -7,47 +10,74 @@ from matplotlib.figure import Figure
 
 app = Tk()
 app.title("Line Coding Visualization")
+app.geometry('1366x700')
 
-app.geometry('750x500')
+top_frame = Frame(app)
+top_frame.pack()
 
-top_space = Canvas(app, width=10, height=25) 
-top_space.grid(row = 0,column = 2)
+Label(top_frame, text='Text').pack()
+e1 = Entry(top_frame, width = 50) 
+e1.pack() 
 
-Label(app, text='Text').grid(row=1) 
-e1 = Entry(app, width = 50) 
-e1.grid(row=1, column=1) 
+bot_top_frame = Frame(top_frame)
+bot_top_frame.pack(side = BOTTOM)
 
-middle_space = Canvas(app, width=10, height=25)
-middle_space.grid(row = 2,column = 2) 
+lbl = Label(bot_top_frame, text="Output here") 
+lbl.pack()
 
-lbl = Label(app, text="Output here") 
-lbl.grid(column=2, row=6)
-
-def AMI(message):
-    output = []
-    change = 1
-    for x in text2binary(message):
-        if (x == '0'):
-            output.append(0)
-        else:
-            output.append(change)
-            if(change == 1):
-                change = -1
-            else:
-                change = 1
-    return output
-
+# AMI button
 def ami():
     res= ''.join(str(x) for x in AMI(e1.get()))
     lbl.configure(text= res)
+    ax1.set_title('AMI')    
+    
+    # panel.pack()
 
-x = Button(app, text='AMI',fg="blue", command = ami)
-x.grid(row = 4,column =1)
+x = Button(bot_top_frame, text='AMI',fg="blue", command = ami)
+x.pack()
 
-exit = Button(app, text='Stop',fg="red" ,width=25, command=app.destroy)
-exit.grid(row = 5)
+# img = ImageTk.PhotoImage(PIL.Image.open("temp.jpg"))
+# panel = Label(app, image = img)
 
-bottom_space = Canvas(app, width=10, height=25)
-bottom_space.grid(row = 4,column = 0) 
+figure1 = plt.Figure(figsize=(6,5), dpi=100)
+ax1 = figure1.add_subplot(111)
+bar1 = FigureCanvasTkAgg(figure1, bot_top_frame)
+bar1.get_tk_widget().pack(fill= BOTH)
+
+
+bottom_frame = Frame(app, height = 400)
+bottom_frame.pack(side = BOTTOM)
+
+
+
+
+
+
+
+# exit
+exit = Button(bottom_frame, text='Stop',fg="red" ,width=25, command=app.destroy)
+exit.pack()
+
+
+
+# top_space = Canvas(app, width=10, height=25) 
+# top_space.grid(row = 0,column = 2)
+
+
+
+# middle_space = Canvas(app, width=10, height=25)
+# middle_space.grid(row = 2,column = 2) 
+
+
+
+
+
+
+# #  
+
+
+
+# bottom_space = Canvas(app, width=10, height=25)
+# bottom_space.grid(row = 7,column = 0) 
 
 app.mainloop()
