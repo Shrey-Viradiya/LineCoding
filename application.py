9e73ddee7cfff91a62d35b5f8860b560e01671aa
tirthasheshpatel <tirthasheshpatel@gmail.com>
2019-10-15 21:12:57 +0530

    Add functionality to embed live graph

--- application.py
+++ application.py
@@ -1,13 +1,20 @@
 from utilities import text2binary
 from line_coding_schemes import AMI
-from PIL import ImageTk
-import PIL.Image
+# from PIL import ImageTk
+# import PIL.Image
 from tkinter import *
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from line_coding_schemes import *
+import utilities as utils
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib import style
+# style.use("dark_background")
 
+fig = plt.figure(figsize=(10,10))
+ax = fig.add_subplot(111)
 app = Tk()
 app.title("Line Coding Visualization")
 app.geometry('1366x700')
@@ -27,32 +34,46 @@ lbl.pack()
 
 # AMI button
 def ami():
-    res= ''.join(str(x) for x in AMI(e1.get()))
-    lbl.configure(text= res)
-    ax1.set_title('AMI')    
-    
-    # panel.pack()
+    message = e1.get()
+    # res= ''.join(str(x) for x in message)
+    lbl.configure(text=message)
+    ax.set_title('AMI')
+    # print(message)
+    code_type = 'AMI'
+    coded_message = AMI(message)
+    ax.clear()
+    ax.text(0.1, 1.3, f"message: {message}, coded: {utils.text2binary(message)}, Coding to {code_type}")
+    ax.set_title('AMI')
+    plt.xticks(range(len(coded_message)))
+    plt.yticks([-2,-1,0,1,2])
+    plt.grid()
+    x_dat = []
+    j=0
+    for i in range(1, len(coded_message)+1):
+        try:
+            ax.plot([j, j+1], [coded_message[i-1], coded_message[i-1]], color='g')
+            if coded_message[i-1] != coded_message[i]:
+                ax.plot([j+1, j+1], [coded_message[i-1], coded_message[i]], color='g')
+        except:
+            ax.plot([j, j+1], [coded_message[i-1], coded_message[i-1]], color='g')
+        j+=1
+    canvas.get_tk_widget().pack(fill= BOTH)
+    canvas.draw()
+
+def animate(i):
+    ami()
 
 x = Button(bot_top_frame, text='AMI',fg="blue", command = ami)
 x.pack()
+ani = FuncAnimation(fig, animate, interval=1000)
+canvas = FigureCanvasTkAgg(fig, bot_top_frame)
+canvas.get_tk_widget().pack(fill= BOTH)
 
 # img = ImageTk.PhotoImage(PIL.Image.open("temp.jpg"))
 # panel = Label(app, image = img)
 
-figure1 = plt.Figure(figsize=(6,5), dpi=100)
-ax1 = figure1.add_subplot(111)
-bar1 = FigureCanvasTkAgg(figure1, bot_top_frame)
-bar1.get_tk_widget().pack(fill= BOTH)
-
-
 bottom_frame = Frame(app, height = 400)
 bottom_frame.pack(side = BOTTOM)
-
-
-
-
-
-
 
 # exit
 exit = Button(bottom_frame, text='Stop',fg="red" ,width=25, command=app.destroy)
