1b5dc9e42ad0979de9750523d36fcccaed159bd9
tirthasheshpatel <tirthasheshpatel@gmail.com>
2019-10-20 12:46:25 +0530

    Remove menifest files

--- main.py
+++
@@ -1,6 +0,0 @@
-from utilities import text2binary
-from linecoding_shrey import polarRZ, AMI, pseudoternary
-from LineCoding_TirthHihoriya import manchester, _2B1Q
-
-print(text2binary('Hello'))
-print(_2B1Q('Hello'))
--- utilities_test.py
+++
@@ -1,9 +0,0 @@
-from utilities import image_to_binary
-import cv2 
-
-img = cv2.imread('small_image.png', 2)
-bw_img = image_to_binary(img)
-# cv2.imshow("binary image", bw_img)
-print(bw_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
